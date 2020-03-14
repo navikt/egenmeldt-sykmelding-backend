0ed651380228cd5f2546a573f181b96cb2f9c91e
Tia Firing <tia.firing@nav.no>
2020-03-14 20:36:22 +0100

    Initielt oppsett

---
+++ src/main/kotlin/no/nav/syfo/Environment.kt
@@ -0,0 +1,26 @@
+package no.nav.syfo
+
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.nio.file.Paths
+import no.nav.syfo.kafka.KafkaConfig
+import no.nav.syfo.kafka.KafkaCredentials
+
+data class Environment(
+    val applicationPort: Int = getEnvVar("APPLICATION_PORT", "8080").toInt(),
+    val applicationName: String = getEnvVar("NAIS_APP_NAME", "egenmeldt-sykmelding-backend"),
+    override val kafkaBootstrapServers: String = getEnvVar("KAFKA_BOOTSTRAP_SERVERS_URL")
+) : KafkaConfig
+
+data class VaultSecrets(
+    val serviceuserUsername: String = getFileAsString("/secrets/serviceuser/username"),
+    val serviceuserPassword: String = getFileAsString("/secrets/serviceuser/password")
+) : KafkaCredentials {
+    override val kafkaUsername: String = serviceuserUsername
+    override val kafkaPassword: String = serviceuserPassword
+}
+
+fun getEnvVar(varName: String, defaultValue: String? = null) =
+        System.getenv(varName) ?: defaultValue ?: throw RuntimeException("Missing required variable \"$varName\"")
+
+fun getFileAsString(filePath: String) = String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.UTF_8)
