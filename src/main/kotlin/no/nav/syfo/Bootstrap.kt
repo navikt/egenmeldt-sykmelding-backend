c16e4063a007ee68f5236c1e36a05ef213b45785
Tia Firing <tia.firing@nav.no>
2020-03-21 09:18:41 +0100

    mer opprydding

--- src/main/kotlin/no/nav/syfo/Bootstrap.kt
+++ src/main/kotlin/no/nav/syfo/Bootstrap.kt
@@ -22,6 +22,7 @@ import no.nav.syfo.arbeidsgivere.integration.arbeidsforhold.client.Arbeidsforhol
 import no.nav.syfo.arbeidsgivere.integration.organisasjon.client.OrganisasjonsinfoClient
 import no.nav.syfo.arbeidsgivere.service.ArbeidsgiverService
 import no.nav.syfo.client.StsOidcClient
+import no.nav.syfo.sykmelding.util.KafkaClients
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
--- src/main/kotlin/no/nav/syfo/sykmelding/util/KafkaClients.kt
+++ src/main/kotlin/no/nav/syfo/sykmelding/util/KafkaClients.kt
@@ -1,11 +1,13 @@
-package no.nav.syfo
+package no.nav.syfo.sykmelding.util
 
+import no.nav.syfo.Environment
+import no.nav.syfo.VaultSecrets
 import no.nav.syfo.kafka.loadBaseConfig
 import no.nav.syfo.kafka.toProducerConfig
 import no.nav.syfo.model.ReceivedSykmelding
 import org.apache.kafka.clients.producer.KafkaProducer
 
-class KafkaClients constructor(env: Environment, vaultSecrets: VaultSecrets) {
+class KafkaClients(env: Environment, vaultSecrets: VaultSecrets) {
 
     private val kafkaBaseConfig = loadBaseConfig(env, vaultSecrets)
     private val producerProperties = kafkaBaseConfig.toProducerConfig(env.applicationName, valueSerializer = JacksonKafkaSerializer::class)
